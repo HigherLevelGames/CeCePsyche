a5184215568b5cee2401610e7b41c995b12cbf75
magicjedi90 <jesse@JessesMacBook.local>
2014-07-15 15:49:36 -0700

    Added in Janelle's background for level one
    added in a temporary script that makes it scroll with the player

---
+++ Assets/Scripts/tempBGScroll.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+var cameraMain : GameObject;
+var increaseY : float;
+function Start () {
+this.transform.position.y = cameraMain.transform.position.y + increaseY;
+
+}
+
+function Update () {
+	this.transform.position.x = cameraMain.transform.position.x;
+	
+}
