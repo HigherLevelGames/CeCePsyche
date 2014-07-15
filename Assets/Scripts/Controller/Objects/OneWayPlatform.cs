416e51e3c9cd1f6051d2f84db8b7b0095f8854c7
Jordan Nguyen <planettop92@Jordans-MacBook-Pro-3.local>
2014-07-15 15:49:54 -0700

    Oneway platforms for the tree

---
+++ Assets/Scripts/Controller/Objects/OneWayPlatform.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OneWayPlatform : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+	{
+		transform.parent.collider2D.isTrigger = false; // JIC
+	}
+
+	void OnTriggerEnter2D(Collider2D player)
+	{
+		// let player pass through
+		this.transform.parent.collider2D.isTrigger = true;
+	}
+	
+	void OnTriggerExit2D(Collider2D player)
+	{
+		// make platform solid again
+		transform.parent.collider2D.isTrigger = false;
+	}
+}
