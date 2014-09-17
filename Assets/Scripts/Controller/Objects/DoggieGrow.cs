80843d29680fe27651e88f356d88638cb0626d26
magicjedi90 <jesse@JessesMacBook.local>
2014-09-17 12:30:26 -0700

    got the dog to grow and shrink

--- Assets/Scripts/Controller/Objects/DoggieGrow.cs
+++ Assets/Scripts/Controller/Objects/DoggieGrow.cs
@@ -2,7 +2,10 @@
 using System.Collections;
 
 public class DoggieGrow : MonoBehaviour {
-
+	public GameObject dog;
+	public float growSize = 0.01f;
+	public float growFrame = -1f;
+	private bool big;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,10 +13,43 @@ public class DoggieGrow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(growFrame >= 0)
+		{
+			if( growFrame >= 1.0f)
+			{
+				growFrame -= Time.deltaTime;
+					dog.transform.localScale += new Vector3 (-growSize,-growSize,0);
+				if(growFrame < 1.0f)
+					growFrame = -1.0f;
+			}
+			else {
+				dog.transform.localScale += new Vector3 (growSize,growSize,0);
+				growFrame += Time.deltaTime;
+			if(growFrame > 1.0f)
+				growFrame = -1.0f;
+			}
+		}
+
 	}
 
 	void Interact() {
-		print("hello");
+		if(big) {
+			print ("small");
+			growFrame = 2;
+			big = false;
+
+			//Vector3 growth = new Vector3 (-growSize,-growSize,0);
+			//dog.transform.localScale +=growth;
+
+
+		}
+
+		else {
+			print ("big");
+			growFrame = 0;
+			//Vector3 growth = new Vector3 (growSize,growSize,0);
+			//dog.transform.localScale += growth;
+			big = true;
+		}
 	}
-}
+} 
