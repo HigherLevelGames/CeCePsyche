c97cbcd00de305e59ae643497a4497418888a95e
Jordan Nguyen <planettop92@Jordans-MacBook-Pro-3.local>
2014-09-24 10:56:47 -0700

    Folder Management
    Hopefully I didn't break too much stuff

---
+++ Assets/Editor/AutoTileEd/AutoTileSort.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+//using System.Collections;
+using UnityEditor;
+
+// JNN: custom hierarchy sort for AutoTile game objects, made possible with Unity 4.5
+public class AutoTileRowSort : BaseHierarchySort
+{
+	public override int Compare(GameObject lhs, GameObject rhs)
+	{
+		if (lhs == rhs) return 0;
+		if (lhs == null) return -1;
+		if (rhs == null) return 1;
+
+		if(lhs.transform.position.y < rhs.transform.position.y)
+		{
+			return -1;
+		}
+		else if(lhs.transform.position.y > rhs.transform.position.y)
+		{
+			return 1;
+		}
+		else if(lhs.transform.position.x < rhs.transform.position.x)
+		{
+			return -1;
+		}
+		else if(lhs.transform.position.x > rhs.transform.position.x)
+		{
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+}
+
+public class AutoTileColumnSort : BaseHierarchySort
+{
+	public override int Compare(GameObject lhs, GameObject rhs)
+	{
+		if (lhs == rhs) return 0;
+		if (lhs == null) return -1;
+		if (rhs == null) return 1;
+		
+		if(lhs.transform.position.x < rhs.transform.position.x)
+		{
+			return -1;
+		}
+		else if(lhs.transform.position.x > rhs.transform.position.x)
+		{
+			return 1;
+		}
+		else if(lhs.transform.position.y < rhs.transform.position.y)
+		{
+			return -1;
+		}
+		else if(lhs.transform.position.y > rhs.transform.position.y)
+		{
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+}
+
+public class AutoTileDiagonalSort : BaseHierarchySort
+{
+	public override int Compare(GameObject lhs, GameObject rhs)
+	{
+		if (lhs == rhs) return 0;
+		if (lhs == null) return -1;
+		if (rhs == null) return 1;
+
+		float left = lhs.transform.position.x + lhs.transform.position.y;
+		float right = rhs.transform.position.x + rhs.transform.position.y;
+		if(left < right)
+		{
+			return -1;
+		}
+		else if(left > right)
+		{
+			return 1;
+		}
+		else if(lhs.transform.position.y < rhs.transform.position.y)
+		{
+			return -1;
+		}
+		else if(lhs.transform.position.y > rhs.transform.position.y)
+		{
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+}
---
+++ Assets/Editor/PrefsEd/nToolsReadMe.txt
@@ -0,0 +1,16 @@
+nTools: A free collection of simple utilities, 
+by Nikkolai Davenport <me@nikkolai.com>
+
+This package adds a Tools menu to the menu bar in Unity. From this Tools menu, the following utilities can be selected:
+
+1. Create Basic Folders
+Creates a basic folder structure for new projects consisting of 22 essential directories.
+
+2. Edit Prefs Utility
+Opens a utility for editing key-value pairs in PlayerPrefs and EditorPrefs.
+
+3. Delete PlayerPrefs
+Deletes all player preferences.
+
+4. Delete EditorPrefs
+Deletes all editor preferences (use with caution).
