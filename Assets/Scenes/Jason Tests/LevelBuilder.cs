818e937ecdec9449b5302e00e04ab1568d800265
Spiris <Jasontheechidna@cox.net>
2014-09-27 00:07:57 +0800

    Level Builder Script

---
+++ Assets/Scenes/Jason Tests/LevelBuilder.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class LevelBuilder : EditorWindow
+{
+		enum EditMode
+		{
+				Walls = 0,
+				Floors = 1,
+				None = 2
+		}
+		EditMode mode = EditMode.Walls;
+
+		[MenuItem("Window/My Window")]
+		public static void ShowWindow ()
+		{
+				EditorWindow.GetWindow (typeof(LevelBuilder));
+
+		}
+
+		void Update ()
+		{
+				EditFloors ();
+		}
+
+		void OnGUI ()
+		{
+				GUILayout.Label ("Modes", EditorStyles.boldLabel);
+				mode = (EditMode)EditorGUILayout.EnumPopup ("Editing Mode", mode);
+		}
+		void EditFloors ()
+		{
+				if (Input.GetMouseButtonDown (0)) {
+						GameObject obj = new GameObject ();
+						obj.AddComponent<BoxCollider2D> ();
+						obj.transform.position = new Vector3 ();
+
+						Instantiate (obj);
+				}
+				if (Input.GetMouseButton (0))
+						;
+		}
+
+		void EditWalls ()
+		{
+
+		}
+}
