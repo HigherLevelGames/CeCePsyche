95658cfd390d61c5fc9a854db42411aa1fa1d599
Spiris <Jasontheechidna@cox.net>
2014-09-27 05:26:39 +0800

    Scrolling Sprites, solved?

---
+++ Assets/Scenes/Jason Tests/ScrollLoopSprite.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollLoopSprite : MonoBehaviour {
+
+	public SpriteRenderer Render1, Render2;
+	public float speed = 0.3f;
+	float offsetx1, offsetx2;
+
+	void Start () {
+		offsetx2 = -0.5f;
+	}
+
+	void Update () {
+		offsetx1 += Time.deltaTime * speed;
+		offsetx2 += Time.deltaTime * speed;
+		if (offsetx2 > 0.5f)
+			offsetx2 -= 1.0f;
+		if (offsetx1 > 0.5f)
+			offsetx1 -= 1.0f;
+		Render1.material.mainTextureOffset = new Vector2 (offsetx1, 0);
+		Render2.material.mainTextureOffset = new Vector2 (offsetx2, 0);
+	}
+}
