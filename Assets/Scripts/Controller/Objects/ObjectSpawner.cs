97a694764542b05cb6945acc558e702cf4cac04e
magicjedi90 <jesse@JessesMacBook.local>
2014-09-26 15:58:18 -0700

    What is love?
    baby don’t hurt me
    don’t hurt me
    no more

---
+++ Assets/Scripts/Controller/Objects/ObjectSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectSpawner : MonoBehaviour {
+	public GameObject objectToSpawn;
+	public float spawnFrequency;
+	float elapsedTime = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		elapsedTime += Time.deltaTime; 
+		print (elapsedTime);
+		if(elapsedTime >= spawnFrequency)
+		{
+			Instantiate(objectToSpawn,this.transform.position, Quaternion.identity);
+			elapsedTime = 0f;
+		}
+	}
+}
