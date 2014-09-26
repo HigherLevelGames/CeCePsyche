428d254deb5b6d5b7bde314909ac6ee75f23ea60
Spiris <Jasontheechidna@cox.net>
2014-09-27 05:59:58 +0800

    fixed?

--- Assets/Scenes/Jason Tests/ScrollLoopSprite.cs
+++ Assets/Scenes/Jason Tests/ScrollLoopSprite.cs
@@ -1,24 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ScrollLoopSprite : MonoBehaviour {
+public class ScrollLoopSprite : MonoBehaviour
+{
 
-	public SpriteRenderer Render1, Render2;
-	public float speed = 0.3f;
-	float offsetx1, offsetx2;
+		public SpriteRenderer Render1, Render2;
+	public float Speed = 0.3f;
+		float offsetx1, offsetx2;
+		float reset;
 
-	void Start () {
-		offsetx2 = -0.5f;
-	}
+		void Start ()
+		{
+				if (Speed > 0) {
+						offsetx2 = -0.5f;
+						reset = 0.5f;
 
-	void Update () {
-		offsetx1 += Time.deltaTime * speed;
-		offsetx2 += Time.deltaTime * speed;
-		if (offsetx2 > 0.5f)
-			offsetx2 -= 1.0f;
-		if (offsetx1 > 0.5f)
-			offsetx1 -= 1.0f;
-		Render1.material.mainTextureOffset = new Vector2 (offsetx1, 0);
-		Render2.material.mainTextureOffset = new Vector2 (offsetx2, 0);
-	}
+				} else {
+						offsetx2 = 0.5f;
+						reset = -0.5f;
+				}
+		}
+
+		void Update ()
+		{
+				offsetx1 += Time.deltaTime * Speed;
+				offsetx2 += Time.deltaTime * Speed;
+				if (Speed > 0) {
+						reset = 0.5f;
+						if (offsetx2 > reset)
+								offsetx2 -= reset * 2;
+						if (offsetx1 > reset)
+								offsetx1 -= reset * 2;
+				} else {
+						reset = -0.5f;
+						if (offsetx2 < reset)
+							offsetx2 -= reset * 2;
+						if (offsetx1 < reset)
+							offsetx1 -= reset * 2;
+				}
+				Render1.material.mainTextureOffset = new Vector2 (offsetx1, 0);
+				Render2.material.mainTextureOffset = new Vector2 (offsetx2, 0);
+		}
 }
