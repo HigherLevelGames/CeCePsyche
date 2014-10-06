3d9751456ea3b8da07b365ff6e0e3fa2a7914b40
magicjedi90 <jesse@jessesmacbook.mobile.asu.edu>
2014-10-06 10:26:41 -0700

    Getting Background to work
    Made background objects that used 3D box colliders so as to not
    interfere with Ceci, also adjusted sorting layers for particle effects

---
+++ Assets/Scripts/Controller/Objects/Platforms/ConveyorBg.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConveyorBg : MonoBehaviour
+{
+	public bool isLeft = true;
+	public float speed = 5.0f;
+	
+	void OnTriggerStay(Collider col)
+	{
+		Vector3 transpose = (isLeft) ? Vector3.left : Vector3.right;
+		col.gameObject.transform.position += transpose * speed * Time.deltaTime;
+	}
+}
