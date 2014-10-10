95dc33d61dd581cc5d8cbf06e8792b0bf3b8807b
magicjedi90 <jesse@JessesMacBook.local>
2014-10-10 11:53:23 -0700

    Attempted Jason's level editor and added in doors
    doors have animations
    started on our first level as well (1_1)

---
+++ Assets/Scripts/Controller/Objects/Door.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door: MonoBehaviour {
+
+	Animator anim;
+	public bool isOpen;
+	// Use this for initialization
+	void Start () {
+		anim = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetBool("isOpen", isOpen);
+	}
+}
