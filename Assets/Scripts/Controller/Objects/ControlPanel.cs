25e91842af1b13fd3bde51b6bc89e50f05d4bc32
magicjedi90 <jesse@JessesMacBook.local>
2014-10-10 13:54:06 -0700

    Added in art, door script and control panel script

---
+++ Assets/Scripts/Controller/Objects/ControlPanel.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlPanel : MonoBehaviour {
+	Animator anim;
+	public AudioClip beepSound;
+	public AudioClip buzzerSound;
+	public AudioSource buzzer;
+	public Door door;
+	public bool isActive;
+	public float activeTime = 1f;
+	float timeLeft;
+
+	// Use this for initialization
+	void Start () {
+		anim = this.GetComponent<Animator>();
+		timeLeft = activeTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetBool("isActive",isActive);
+		if(isActive)
+		{
+			timeLeft -= Time.deltaTime;
+			if (timeLeft < 0)
+			{
+				isActive = false;
+				timeLeft = activeTime;
+			}
+		}
+	}
+
+	void Interact () {
+		if(!isActive)
+		{
+			isActive = true;
+		}
+		if(door != null)
+		{
+			bool isOpen = door.isOpen;
+			bool autoClose = door.autoClose;
+			if(isOpen && !autoClose)
+			{
+				door.isOpen = false;
+			}
+
+			if(!isOpen)
+			{
+				door.isOpen = true;
+			}
+		}
+
+		if(buzzer != null)
+		{
+		}
+	}
+}
--- Assets/Scripts/Controller/Objects/Door.cs
+++ Assets/Scripts/Controller/Objects/Door.cs
@@ -2,16 +2,32 @@
 using System.Collections;
 
 public class Door: MonoBehaviour {
+	
+	public bool isOpen;
+	public bool autoClose = false;
+	public float closeTime = 5f;
 
 	Animator anim;
-	public bool isOpen;
+	float timeLeft;
+
 	// Use this for initialization
 	void Start () {
 		anim = this.GetComponent<Animator>();
+		timeLeft = closeTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		anim.SetBool("isOpen", isOpen);
+		if(isOpen && autoClose)
+		{
+			timeLeft -= Time.deltaTime;
+			if(timeLeft < 0)
+			{
+				timeLeft = closeTime;
+				isOpen = false;
+			}
+		}
+
 	}
 }
