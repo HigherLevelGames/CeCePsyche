d7e76979fdd1687b989e359dba84e5046db08816
magicjedi90 <jesse@JessesMacBook.local>
2014-10-17 16:24:58 -0700

    walk animations and destructible objects

--- Assets/Scenes/Jesse Tests/Destructable.cs
+++ Assets/Scenes/Jesse Tests/Destructable.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Destroyable : MonoBehaviour {
+public class Destructable : MonoBehaviour {
+	public GameObject destroyedBy;
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +11,18 @@ public class Destroyable : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		print (col.gameObject.ToString());
+		print (destroyedBy.gameObject.ToString()+"(Clone)");
+		GameObject stuff = col.gameObject;
+		stuff.name = stuff.name.Substring(0,stuff.name.Length-7); // remove "(Clone)" at end of name
+		if(stuff.ToString() == destroyedBy.ToString())
+		{
+			Destroy(this.gameObject);
+		}
 	}
 }
