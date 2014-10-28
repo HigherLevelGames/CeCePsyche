7e7cc13bcdf0573588ee6620b721f7abaa036337
Spiris <Jasontheechidna@cox.net>
2014-10-29 05:22:33 +0800

    Alot of buggy stuff with shaders and such. Slight Psymode tweaks.

---
+++ Assets/Scenes/Jason Tests/GlowableSprite.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlowableSprite : MonoBehaviour
+{
+    public enum Method
+    {
+        None = 0,
+        BehindSprite = 1,
+        Sprite = 2,
+        Both = 3
+    }
+    public SpriteRenderer TargetRenderer;
+    public Method GlowMethod;
+    public Material alternateMaterial;
+    public float Value;
+
+    GameObject backSprite;
+    SpriteRenderer bspr;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        if (!TargetRenderer)
+        {
+            Debug.Log("The script GlowableSprite is missing a public variable assignment.");
+            return;
+        }
+        switch (GlowMethod)
+        {
+            case Method.None:
+                return;
+            case Method.BehindSprite:
+                SetupRenderers();
+                return;
+            case Method.Sprite:
+                return;
+            case Method.Both:
+                SetupRenderers();
+                return;
+
+        }
+
+
+    }
+    void SetupRenderers()
+    {
+        Sprite sp = TargetRenderer.sprite;
+        backSprite = new GameObject();
+        bspr = backSprite.AddComponent<SpriteRenderer>();
+        bspr.color = new Color(1, 1, 1, 1);
+        bspr.sprite = sp;
+        bspr.material = alternateMaterial;
+        bspr.sortingLayerID = TargetRenderer.sortingLayerID;
+        bspr.sortingOrder = TargetRenderer.sortingOrder - 1;
+        float oldWidth = sp.bounds.extents.x;
+        float oldHeight = sp.bounds.extents.y;
+        backSprite.name = "haloglow";
+        backSprite.transform.parent = this.transform;
+        backSprite.transform.position = new Vector3(this.transform.position.x - oldWidth * 0.05f, this.transform.position.y - oldHeight * 0.05f, 0);
+        backSprite.transform.localScale = new Vector3(1.1f, 1.1f, 1f);
+    }
+
+    bool reverse;
+
+    void Update()
+    {
+        if (!TargetRenderer)
+            return;
+        if (Value < 0)
+            reverse = true;
+        else if (Value > 1)
+            reverse = false;
+        if (reverse)
+            Value += Time.deltaTime;
+        else
+            Value -= Time.deltaTime;
+        switch (GlowMethod)
+        {
+            case Method.None:
+                return;
+            case Method.BehindSprite:
+                bspr.color = new Color(1, 1, 1, Value);
+                return;
+            case Method.Sprite:
+                TargetRenderer.material.SetFloat("_GlowRate", Value);
+                return;
+            case Method.Both:
+                bspr.color = new Color(1, 1, 1, Value);
+                TargetRenderer.material.SetFloat("_GlowRate", Value);
+                return;
+        }
+    }
+}
--- Assets/Scenes/Jason Tests/PsyModeController.cs
+++ Assets/Scenes/Jason Tests/PsyModeController.cs
@@ -6,7 +6,12 @@ public class PsyModeController : MonoBehaviour
    
     public GameObject Player;
     public GameObject Other;
-    public Texture[] textures;
+    public Material HighlightSprite;
+    public Texture[] Neutrals;
+    public Texture[] Environmentals;
+    public Texture[] Responses;
+    public int SelectedStimuli = 0;
+    PsyModeStimuli[] stimuli = new PsyModeStimuli[32];
     SpriteRenderer spr;
     Camera cam;
     float localScalar = 1.0f;
@@ -18,8 +23,10 @@ public class PsyModeController : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        for (int i = 0; i < stimuli.Length; i++)
+            stimuli [i] = new PsyModeStimuli(new GameObject(), CCType.NeutralStimuli);
         cam = this.GetComponentInParent<Camera>();
-        spr = this.GetComponent<SpriteRenderer>();
+        spr = this.GetComponentInChildren<SpriteRenderer>();
         spr.color = new Color(c.r, c.g, c.b, c.a * antiScalar);
     }
 
@@ -38,59 +45,96 @@ public class PsyModeController : MonoBehaviour
         }
         Time.timeScale = localScalar;
         spr.color = new Color(c.r, c.g, c.b, c.a * antiScalar);
+        for (int i = 0; i < stimuli.Length; i++)
+        {
+            switch (stimuli [i].Type)
+            {
+                default:
+                    break;
+            }
+        }
     }
 
     void OnGUI()
     {
+
         if (localScalar < 1.0f)
         {
-            Vector2 p = cam.WorldToScreenPoint(Player.transform.position).ToVector2() + new Vector2(100, 100);
-            p = new Vector2(Mathf.Clamp(p.x, 100, Screen.width - 100), Mathf.Clamp(Screen.height - p.y, 100, Screen.width - 100));
-            Vector2 o = cam.WorldToScreenPoint(Other.transform.position).ToVector2();
             float xa = 75 * antiScalar;
+            Vector2 p = cam.WorldToScreenPoint(Player.transform.position).ToVector2();
+            p += new Vector2(xa, xa + 20);
+            p = new Vector2(Mathf.Clamp(p.x, 100, Screen.width - 100), Mathf.Clamp(Screen.height - p.y, 100, Screen.width - 100));
+
             // Draw selectable stimuli next to Ceci
-            GUI.Window(0, new Rect(p.x - xa, p.y - xa, xa * 2, xa * 2), PlayerWindow, "Stimuli", GUIStyle.none);
+            DrawNeutralStimuli(p);
+            //GUI.Window(0, new Rect(p.x - xa, p.y - xa, xa * 2, xa * 2), PlayerWindow, "Stimuli", GUIStyle.none);
             // Draw unconditioned response next to target
+            Vector2 o = cam.WorldToScreenPoint(Other.transform.position).ToVector2();
             float xd = antiScalar * 25;
-            GUI.Button(new Rect(o.x - xd, Screen.height - o.y - xd, xd * 2, xd * 2), "Response");
+            GUI.DrawTexture(new Rect(o.x - xd, Screen.height - o.y - xd, xd * 2, xd * 2), Responses [0]);
             // Draw environmental/unconditioned stimuli
-            GUI.Window(1, new Rect(-5, -3, Screen.width + 10, Screen.height * 0.1f * antiScalar), EnvironmentWindow, "Environmental", GUIStyle.none);
+            EnvironmentWindow();
 
         }         
     }
 
-    void EnvironmentWindow(int id)
+    void DrawNeutralStimuli(Vector2 p)
     {
-        int numStim = 25;
+        float rad = 34.0f * antiScalar;
+        float wa = 60 * antiScalar;
+        float wb = 30 * antiScalar;
+        float bonusRotation = Mathf.PI * 2 * antiScalar - Mathf.PI * 0.75f;
+        for (int i = 0; i < Neutrals.Length; i++)
+        {
+            float r = ((float)i / Neutrals.Length) * Mathf.PI * 2 * antiScalar + bonusRotation;
+            float x = p.x + rad * (Mathf.Cos(r) - Mathf.Sin(r));
+            float y = p.y + rad * (Mathf.Sin(r) + Mathf.Cos(r));
+            Rect rec = new Rect(x - wb, y - wb, wa, wa);
+            if (i == SelectedStimuli)
+            {
+                //glowSprite.Glow(rec, Neutrals [i], HighlightSprite);
+            } else
+                Graphics.DrawTexture(rec, Neutrals [i]);
+        }
+    }
+
+    void EnvironmentWindow()
+    {
+        int numStim = 5;
+        float w = 60.0f;
+        float wa = w * antiScalar;
 
         for (int i = 0; i < numStim; i++)
         {
-            float r = ((float)i / numStim);
-            float t = Mathf.Sin(r * Mathf.PI * 2);
-            float x = 20 + r * Screen.width;
-            float n = (i + 1) * 60 * t;
-            float y = n * antiScalar - n + 10;
-            GUI.Button(new Rect(x, y, 70, 55), i.ToString());
+            float r = (float)(i + 1) / numStim;
+
+            float wn = wa * r - w * r + w;
+            float wb = wn * 0.5f;
+
+            float x = 30 + i * w * 0.8f;
+            float y = 30 + wb * (i % 2);
+
+            GUI.DrawTexture(new Rect(x - wb, y - wb, wn, wn), Environmentals [i % Environmentals.Length]);
         }
     }
 
     void PlayerWindow(int id)
     {
-        int numButtons = 3;
-        int numDiff = 4;
+        int numStim = 6;
         float rad = 34.0f * antiScalar;
         Vector2 center = new Vector2(75 * antiScalar, 80 * antiScalar);
         float wa = 60 * antiScalar;
         float wb = 30 * antiScalar;
         float bonusRotation = Mathf.PI * 2 * antiScalar - Mathf.PI * 0.75f;
-        for(int i = 0; i < numButtons; i++)
+        for (int i = 0; i < numStim; i++)
         {
-            float r = ((float)i / numButtons) * Mathf.PI * 2 * antiScalar + bonusRotation;
-            float x = center.x + rad*(Mathf.Cos(r) - Mathf.Sin(r));
-            float y = center.y + rad*(Mathf.Sin(r) + Mathf.Cos(r));
+            float r = ((float)i / numStim) * Mathf.PI * 2 * antiScalar + bonusRotation;
+            float x = center.x + rad * (Mathf.Cos(r) - Mathf.Sin(r));
+            float y = center.y + rad * (Mathf.Sin(r) + Mathf.Cos(r));
             Rect rec = new Rect(x - wb, y - wb, wa, wa);
-            GUI.DrawTexture(rec, textures[i % numDiff]);
+            GUI.DrawTexture(rec, Neutrals [i % Neutrals.Length]);
         }
+
     }
 
     void EnablePsyMode()
@@ -103,4 +147,60 @@ public class PsyModeController : MonoBehaviour
     {
         psyOn = false;
     }
+}
+
+public enum CCType
+{
+    NeutralStimuli,
+    ConditionedStimuli,
+    UnconditionedStimuli,
+    UnconditionedResponse,
+    ConditionedResponse
+}
+
+public class PsyModeStimuli
+{
+    public bool IsGlowing = false;
+    public CCType Type;
+    GameObject g;
+    SpriteRenderer spr;
+    bool reverse;
+    float a;
+
+    public PsyModeStimuli(GameObject g, CCType type)
+    {
+        this.g = g;
+        this.spr = g.GetComponent<SpriteRenderer>();
+        this.Type = type;
+    }
+
+    void DrawAt(Vector2 p)
+    {
+
+    }
+
+    public void Glow()
+    {
+        if (a > 1)
+            reverse = true;
+        if (a < 0)
+            reverse = false;
+        if (reverse)
+            a -= Time.unscaledDeltaTime;
+        else
+            a += Time.unscaledDeltaTime;
+        spr.material.SetFloat("_GlowRate", a);
+    }
+}
+
+public class GlowSprite
+{
+    float a;
+    bool reverse;
+
+    public GlowSprite()
+    {
+
+    }
+
 }
