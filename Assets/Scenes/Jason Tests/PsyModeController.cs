bae24034b6c8d6a115f994679d317d2cb6fff0df
Spiris <Jasontheechidna@cox.net>
2014-10-30 11:58:28 +0800

    buggy psymode, do not use.

--- Assets/Scenes/Jason Tests/PsyModeController.cs
+++ Assets/Scenes/Jason Tests/PsyModeController.cs
@@ -1,33 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+public enum PMType
+{
+    Neutral,
+    Unconditioned,
+    Response
+}
 
 public class PsyModeController : MonoBehaviour
 {
-   
     public GameObject Player;
-    public GameObject Other;
-    public Material HighlightSprite;
-    public Texture[] Neutrals;
-    public Texture[] Environmentals;
-    public Texture[] Responses;
-    public int SelectedStimuli = 0;
-    PsyModeStimuli[] stimuli = new PsyModeStimuli[32];
-    SpriteRenderer spr;
+    public GameObject[] Others;
+    public GameObject[] Neutrals;
+    public GameObject[] Environmentals;
+    public GameObject[] Responses;
+    SpriteRenderer bgrender;
     Camera cam;
-    float localScalar = 1.0f;
+    float localScalar = 0;
 
     float antiScalar { get { return 1.0f - localScalar; } }
 
     Color c = new Color(0, 0, 1.0f, 0.7f);
+    PsyModeSlot[] menuSlot = new PsyModeSlot[21];
+    ResponseSlot[] targets;
+    List<PsyModeObject> psyObjects = new List<PsyModeObject>();
+    bool cooling;
     bool psyOn;
-    // Use this for initialization
+
     void Start()
     {
-        for (int i = 0; i < stimuli.Length; i++)
-            stimuli [i] = new PsyModeStimuli(new GameObject(), CCType.NeutralStimuli);
+
+        for (int i = 0; i < menuSlot.Length; i++)
+            menuSlot [i] = new PsyModeSlot();
+        AddPsyObject(Instantiate(Neutrals [0]) as GameObject, this.transform, PMType.Neutral, 0);
+        AddPsyObject(Instantiate(Environmentals [0]) as GameObject, this.transform, PMType.Unconditioned, 12);
+        AddPsyObject(Instantiate(Responses [0]) as GameObject, this.transform, PMType.Response, 20);
         cam = this.GetComponentInParent<Camera>();
-        spr = this.GetComponentInChildren<SpriteRenderer>();
-        spr.color = new Color(c.r, c.g, c.b, c.a * antiScalar);
+        bgrender = this.GetComponentInChildren<SpriteRenderer>();
+        bgrender.color = new Color(c.r, c.g, c.b, c.a * antiScalar);
+    }
+
+    ResponseSlot[] FindPossibleTargets()
+    {
+        List<ResponseSlot> slots = new List<ResponseSlot>();
+        for (int i = 0; i < Others.Length; i++)
+        {
+            Vector2 p = cam.WorldToViewportPoint(new Vector3(Others [i].transform.position.x, Others [i].transform.position.y, 0));
+            if (p.x > 0 && p.x < 1 && p.y > 0 && p.y < 1)
+            {
+                ResponseSlot rs = new ResponseSlot();
+                rs.position = Others [i].transform.position.ToVector2();
+                // Find responses to draw
+                rs.ItemID = 0;
+                rTarget = 0;
+                slots.Add(rs);
+            }
+        }
+        return slots.ToArray();
     }
 
     void Update()
@@ -35,172 +66,278 @@ public class PsyModeController : MonoBehaviour
         if (psyOn)
         {
             if (Input.GetKeyDown(KeyCode.Z))
-                DisablePsyMode();
-            localScalar = Mathf.Max(0, localScalar * 0.90f);
+                CloseMenu();
+            if (cooling)
+            {
+                localScalar -= Time.unscaledDeltaTime * 2;
+                if (localScalar < 0.0f)
+                {
+                    psyOn = false;
+                    cooling = false;
+                    for (int i = 0; i < psyObjects.Count; i++)
+                        psyObjects [i].SetActive(false);
+                    localScalar = 0;
+                }
+            } else if (localScalar < 1.0f)
+                localScalar = Mathf.Min(1, localScalar + Time.unscaledDeltaTime * 2);
+            if (rTarget > -1)
+            {
+                Vector2 op = targets [rTarget].position;
+                Vector2 pp = Player.transform.position.ToVector2();
+                Vector2 ep = cam.ScreenToWorldPoint(new Vector3(Screen.width * 0.02f, Screen.height * 0.95f, 10));
+                if (Input.GetKeyDown(KeyCode.X))
+                    Confirm();
+                if (Input.GetKeyDown(KeyCode.A))
+                    GetNextSlot();
+                if (Input.GetKeyDown(KeyCode.D))
+                    GetPrevSlot();
+                SetPositions(pp, op, ep, 0.8f);
+                for (int i = 0; i < psyObjects.Count; i++)
+                    psyObjects [i].Update(localScalar);
+            }
         } else
         {
             if (Input.GetKeyDown(KeyCode.Z))
-                EnablePsyMode();
-            localScalar = Mathf.Min(1.0f, 0.1f + localScalar * 1.02f);
+                WarmMenu();
         }
-        Time.timeScale = localScalar;
-        spr.color = new Color(c.r, c.g, c.b, c.a * antiScalar);
-        for (int i = 0; i < stimuli.Length; i++)
+
+
+        Time.timeScale = antiScalar;
+        bgrender.color = new Color(c.r, c.g, c.b, c.a * localScalar);
+    }
+
+    /// <summary>
+    ///  BOOKMARKED SPOT
+    /// </summary>
+    void DrawResponseSlots()
+    {
+        for (int i = 0; i < targets.Length; i++)
         {
-            switch (stimuli [i].Type)
+            for (int j = 0; j < targets[i].Slots.Length; j++)
             {
-                default:
-                    break;
+
             }
         }
+    }
+    enum MenuStage
+    {
+        Target,
+        Environment,
+        Apply
+    }
+    MenuStage stage = MenuStage.Target;
+
+    void Confirm()
+    {
+        switch (stage)
+        {
+            case MenuStage.Target:
+                nTarget = 12;
+                break;
+            case MenuStage.Environment:
+                break;
+            case MenuStage.Apply:
+                break;
+            
+        }
+    }
+
+    int rTarget;
+    int uTarget;
+    int nTarget;
+
+    void GetNextSlot()
+    {
+        int n;
+        switch (stage)
+        {
+            case MenuStage.Target:
+                rTarget = Mathf.Min(rTarget + 1, Others.Length - 1);
+                break;
+            case MenuStage.Environment:
+                n = nTarget + 1;
+                if (menuSlot [n].ItemID > -1 && n < 20)
+                    nTarget++;
+                break;
+            case MenuStage.Apply:
+                break;
+                
+        }
+    }
+
+    void GetPrevSlot()
+    {
+        int n;
+        switch (stage)
+        {
+            case MenuStage.Target:
+                rTarget = Mathf.Max(rTarget - 1, 0);
+                break;
+            case MenuStage.Environment:
+                n = nTarget - 1;
+                if (menuSlot [n].ItemID > -1 && n > 5)
+                    nTarget = n;
+                break;
+            case MenuStage.Apply:
+                break;
+                
+        }
+    }
+
+    void AddPsyObject(GameObject g, Transform parent, PMType t, int atID)
+    {
+        g.transform.parent = parent;
+        psyObjects.Add(new PsyModeObject(g, t));
+        menuSlot [atID].ItemID = psyObjects.Count - 1;
+    }
+
+    void SetPositions(Vector2 pp, Vector2 op, Vector2 ep, float rad)
+    {
+        float srad = rad * localScalar;
+        float bonusRotation = Mathf.PI * 2 * localScalar;
+        for (int i = 0; i < menuSlot.Length; i++)
+        {
+            
+            float r;
+            float x; 
+            float y; 
+            if (i < 6)
+            {
+                r = ((float)i / 6) * Mathf.PI * 2 * localScalar + bonusRotation;
+                x = srad * (Mathf.Cos(r) - Mathf.Sin(r));
+                y = srad * (Mathf.Sin(r) + Mathf.Cos(r));
+                menuSlot [i].position = new Vector2(pp.x + x, pp.y + y);
+            } else if (i < 12)
+            {
+                r = ((float)i / 6) * Mathf.PI * 2 * localScalar + bonusRotation;
+                x = srad * (Mathf.Cos(r) - Mathf.Sin(r));
+                y = srad * (Mathf.Sin(r) + Mathf.Cos(r));
+                menuSlot [i].position = new Vector2(op.x + x, op.y + y);
+            } else if (i < menuSlot.Length - 1)
+            {
+                x = i - 11;
+                r = Mathf.Cos(x * Mathf.PI / (menuSlot.Length - 12));
+                y = r - r * localScalar;
+                menuSlot [i].position = new Vector2(ep.x + x, ep.y + y);
+            } else
+                menuSlot [i].position = op;
+            if (menuSlot [i].ItemID > -1)
+                psyObjects [menuSlot [i].ItemID].Target = menuSlot [i].position;
+        }
+    }
+
+    void WarmMenu()
+    {
+
+        stage = MenuStage.Target;
+        psyOn = true;
+        cooling = false;
+        for (int i = 0; i < psyObjects.Count; i++)
+            psyObjects [i].SetActive(true);
+        targets = FindPossibleTargets();
+        if (targets.Length < 1)
+            rTarget = -1;
+    }
+    
+    void CloseMenu()
+    {
+        cooling = true;
     }
 
     void OnGUI()
     {
-
-        if (localScalar < 1.0f)
+        if (rTarget < 0 && psyOn)
         {
-            float xa = 75 * antiScalar;
-            Vector2 p = cam.WorldToScreenPoint(Player.transform.position).ToVector2();
-            p += new Vector2(xa, xa + 20);
-            p = new Vector2(Mathf.Clamp(p.x, 100, Screen.width - 100), Mathf.Clamp(Screen.height - p.y, 100, Screen.width - 100));
-
-            // Draw selectable stimuli next to Ceci
-            DrawNeutralStimuli(p);
-            //GUI.Window(0, new Rect(p.x - xa, p.y - xa, xa * 2, xa * 2), PlayerWindow, "Stimuli", GUIStyle.none);
-            // Draw unconditioned response next to target
-            Vector2 o = cam.WorldToScreenPoint(Other.transform.position).ToVector2();
-            float xd = antiScalar * 25;
-            GUI.DrawTexture(new Rect(o.x - xd, Screen.height - o.y - xd, xd * 2, xd * 2), Responses [0]);
-            // Draw environmental/unconditioned stimuli
-            EnvironmentWindow();
-
-        }         
-    }
-
-    void DrawNeutralStimuli(Vector2 p)
-    {
-        float rad = 34.0f * antiScalar;
-        float wa = 60 * antiScalar;
-        float wb = 30 * antiScalar;
-        float bonusRotation = Mathf.PI * 2 * antiScalar - Mathf.PI * 0.75f;
-        for (int i = 0; i < Neutrals.Length; i++)
-        {
-            float r = ((float)i / Neutrals.Length) * Mathf.PI * 2 * antiScalar + bonusRotation;
-            float x = p.x + rad * (Mathf.Cos(r) - Mathf.Sin(r));
-            float y = p.y + rad * (Mathf.Sin(r) + Mathf.Cos(r));
-            Rect rec = new Rect(x - wb, y - wb, wa, wa);
-            if (i == SelectedStimuli)
-            {
-                //glowSprite.Glow(rec, Neutrals [i], HighlightSprite);
-            } else
-                Graphics.DrawTexture(rec, Neutrals [i]);
+            GUI.Label(new Rect(Screen.width * 0.5f, Screen.height * 0.5f, 100, 50), "No Targets Found.");
         }
     }
-
-    void EnvironmentWindow()
+    #region Debug
+    void OnDrawGizmos()
     {
-        int numStim = 5;
-        float w = 60.0f;
-        float wa = w * antiScalar;
-
-        for (int i = 0; i < numStim; i++)
-        {
-            float r = (float)(i + 1) / numStim;
-
-            float wn = wa * r - w * r + w;
-            float wb = wn * 0.5f;
-
-            float x = 30 + i * w * 0.8f;
-            float y = 30 + wb * (i % 2);
-
-            GUI.DrawTexture(new Rect(x - wb, y - wb, wn, wn), Environmentals [i % Environmentals.Length]);
-        }
+        if (psyOn)
+            for (int i = 0; i < menuSlot.Length; i++)
+                Gizmos.DrawIcon(menuSlot [i].position.ToVector3(), "icon.tiff");
     }
+    #endregion
+}
 
-    void PlayerWindow(int id)
+public class PsyModeSlot
+{
+    public Vector2 position;
+    public int ItemID = -1;
+}
+
+public class ResponseSlot : PsyModeSlot
+{
+    public PsyModeSlot[] Slots = new PsyModeSlot[6];
+
+    public ResponseSlot()
     {
-        int numStim = 6;
-        float rad = 34.0f * antiScalar;
-        Vector2 center = new Vector2(75 * antiScalar, 80 * antiScalar);
-        float wa = 60 * antiScalar;
-        float wb = 30 * antiScalar;
-        float bonusRotation = Mathf.PI * 2 * antiScalar - Mathf.PI * 0.75f;
-        for (int i = 0; i < numStim; i++)
-        {
-            float r = ((float)i / numStim) * Mathf.PI * 2 * antiScalar + bonusRotation;
-            float x = center.x + rad * (Mathf.Cos(r) - Mathf.Sin(r));
-            float y = center.y + rad * (Mathf.Sin(r) + Mathf.Cos(r));
-            Rect rec = new Rect(x - wb, y - wb, wa, wa);
-            GUI.DrawTexture(rec, Neutrals [i % Neutrals.Length]);
-        }
-
-    }
-
-    void EnablePsyMode()
-    {
-        psyOn = true;
-
-    }
-
-    void DisablePsyMode()
-    {
-        psyOn = false;
+        for (int i = 0; i < Slots.Length; i++)
+            Slots [i] = new PsyModeSlot();
     }
 }
 
-public enum CCType
+public class PsyModeObject
 {
-    NeutralStimuli,
-    ConditionedStimuli,
-    UnconditionedStimuli,
-    UnconditionedResponse,
-    ConditionedResponse
-}
-
-public class PsyModeStimuli
-{
-    public bool IsGlowing = false;
-    public CCType Type;
+    public PMType Type;
+    float lerpScalar = 1.0f;
+    bool reverse;
+    float glowValue;
     GameObject g;
     SpriteRenderer spr;
-    bool reverse;
-    float a;
+    public Vector2 Target;
+    Vector2 old;
 
-    public PsyModeStimuli(GameObject g, CCType type)
+    public PsyModeObject(GameObject g, PMType t)
     {
         this.g = g;
         this.spr = g.GetComponent<SpriteRenderer>();
-        this.Type = type;
+        this.Type = t;
+        old = Target = g.transform.position;
     }
 
-    void DrawAt(Vector2 p)
+    public void Update(float scalar)
     {
-
+        if (lerpScalar < 1.0f)
+        {
+            position = Vector2.Lerp(old, Target, lerpScalar);
+            lerpScalar += Time.unscaledDeltaTime;
+        } else
+        {
+            lerpScalar = 1.0f;
+            position = Target;
+        }
+        g.transform.localScale = Vector3.one * scalar;
     }
 
     public void Glow()
     {
-        if (a > 1)
+        if (glowValue > 1)
             reverse = true;
-        if (a < 0)
+        if (glowValue < 0)
             reverse = false;
         if (reverse)
-            a -= Time.unscaledDeltaTime;
+            glowValue -= Time.unscaledDeltaTime;
         else
-            a += Time.unscaledDeltaTime;
-        spr.material.SetFloat("_GlowRate", a);
+            glowValue += Time.unscaledDeltaTime;
+        spr.material.SetFloat("_GlowRate", glowValue);
     }
-}
 
-public class GlowSprite
-{
-    float a;
-    bool reverse;
-
-    public GlowSprite()
+    public void SetActive(bool val)
     {
-
+        g.SetActive(val);
     }
 
+    public void StartMove()
+    {
+        old = position;
+        lerpScalar = 0.0f;
+    }
+
+    public Vector2 position
+    {
+        get { return g.transform.position.ToVector2(); }
+        set { g.transform.position = value; }
+    }
 }
