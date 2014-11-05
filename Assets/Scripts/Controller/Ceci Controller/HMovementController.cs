e1a8110e1cdd22418ec27975642b3285b0e963dd
Spiris <Jasontheechidna@cox.net>
2014-11-06 04:05:41 +0800

    Added remote control functionality to HMovementController

--- Assets/Scripts/Controller/Ceci Controller/HMovementController.cs
+++ Assets/Scripts/Controller/Ceci Controller/HMovementController.cs
@@ -3,91 +3,106 @@ using System.Collections;
 
 public class HMovementController : MonoBehaviour
 {
-	// hspeed for CeciAnimControl.cs
-	public int hSpeed
-	{
-		get
-		{
-			if(Mathf.Abs(newX-prevX) < 0.05f)
-			{
-				return 0;
-			}
-			else
-			{
-				return (int)Mathf.Sign(newX-prevX);
-			}
-		}
-	}
-
-	public float MaxSpeed = 10.0f;
-	public bool isFacingRight = true; // default, public for Anger Ability to reference
-	private Quaternion reverseRotation = new Quaternion(0.0f,180.0f,0.0f,0.0f);
-	private float prevX = 0.0f;
-	private float newX = 0.0f;
-
-	void Start() {}
-	
-	void FixedUpdate()
-	{
-		Movement();
-
-		// temp vars for storing camera rotation/position
-		// since we made the camera a child of Ceci
-		Quaternion tempRot = Camera.main.transform.rotation;
-		Vector3 tempPos = Camera.main.transform.position;
-
-		FaceDirection();
-
-		// give temp vars back to camera
-		Camera.main.transform.rotation = tempRot;
-		Camera.main.transform.position = tempPos;
-	}
-
-	void OnDrawGizmos()
-	{
-		Gizmos.color = Color.yellow;
-		Gizmos.DrawSphere(checker.pt1, 0.1f);
-		Gizmos.color = Color.green;
-		Gizmos.DrawSphere(checker.pt2, 0.1f);
-		Gizmos.color = Color.blue;
-		Gizmos.DrawSphere(checker.pt3, 0.1f);
-	}
-
-	GroundDetect checker = new GroundDetect();
-	void Movement()
-	{
-		prevX = this.transform.position.x;
-
-		// velocity = speed + direction
-		newX = prevX + RebindableInput.GetAxis("Horizontal") * MaxSpeed * Time.deltaTime;
-		if(RebindableInput.GetAxis("Horizontal") != 0) // check needed in case standing still
-		{
-			isFacingRight = RebindableInput.GetAxis("Horizontal") > 0;
-		}
-
-		float newY = this.transform.position.y;
-		// consideration for slopes
-		if(RebindableInput.GetAxis("Horizontal") != 0
-				&& RebindableInput.GetAxis("Vertical") == 0
-				&& !RebindableInput.GetKeyDown("Jump") )
-		{
-			newY = checker.checkForward(this.transform, newX);
-		}
-
-		// actually move Ceci
-		this.transform.position = new Vector2(newX, newY);
-	}
-
-	void FaceDirection()
-	{
-		// face left or right by changing the y rotation value
-		if(isFacingRight)
-		{
-			this.transform.rotation = Quaternion.identity;
-		}
-		else
-		{
-			this.transform.rotation = reverseRotation;
-		}
-	}
+    // hspeed for CeciAnimControl.cs
+    public int hSpeed
+    {
+        get
+        {
+            if(Mathf.Abs(newX-prevX) < 0.05f)
+            {
+                return 0;
+            }
+            else
+            {
+                return (int)Mathf.Sign(newX-prevX);
+            }
+        }
+    }
+    
+    public float MaxSpeed = 10.0f;
+    public bool isFacingRight = true; // default, public for Anger Ability to reference
+    private Quaternion reverseRotation = new Quaternion(0.0f,180.0f,0.0f,0.0f);
+    private float prevX = 0.0f;
+    private float newX = 0.0f;
+    public bool Left, Right, Remote;
+    void Start() {}
+    
+    void FixedUpdate()
+    {
+        Movement();
+        
+        // temp vars for storing camera rotation/position
+        // since we made the camera a child of Ceci
+        Quaternion tempRot = Camera.main.transform.rotation;
+        Vector3 tempPos = Camera.main.transform.position;
+        
+        FaceDirection();
+        
+        // give temp vars back to camera
+        Camera.main.transform.rotation = tempRot;
+        Camera.main.transform.position = tempPos;
+    }
+    
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(checker.pt1, 0.1f);
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(checker.pt2, 0.1f);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(checker.pt3, 0.1f);
+    }
+    
+    GroundDetect checker = new GroundDetect();
+    void Movement()
+    {
+        prevX = this.transform.position.x;
+        float newY = this.transform.position.y;
+        if (Remote)
+        {
+            if(Right)
+            {
+                isFacingRight = true;
+                newX = prevX + 1 * MaxSpeed * Time.deltaTime;
+                
+            }else if(Left)
+            {
+                isFacingRight = false;
+                newX = prevX - 1 * MaxSpeed * Time.deltaTime;
+            }
+            newY = checker.checkForward(this.transform, newX);
+            
+        } else
+        {
+            // velocity = speed + direction
+            newX = prevX + RebindableInput.GetAxis("Horizontal") * MaxSpeed * Time.deltaTime;
+            if(RebindableInput.GetAxis("Horizontal") != 0) // check needed in case standing still
+            {
+                isFacingRight = RebindableInput.GetAxis("Horizontal") > 0;
+            }
+            
+            // consideration for slopes
+            if(RebindableInput.GetAxis("Horizontal") != 0
+               && RebindableInput.GetAxis("Vertical") == 0
+               && !RebindableInput.GetKeyDown("Jump") )
+            {
+                newY = checker.checkForward(this.transform, newX);
+            }
+        }
+        // actually move Ceci
+        this.transform.position = new Vector2(newX, newY);
+    }
+    
+    void FaceDirection()
+    {
+        // face left or right by changing the y rotation value
+        if(isFacingRight)
+        {
+            this.transform.rotation = Quaternion.identity;
+        }
+        else
+        {
+            this.transform.rotation = reverseRotation;
+        }
+    }
 }
