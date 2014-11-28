4ef4b736994f4dbc473f39ea34840e057fd00018
magicjedi90 <jesse@JessesMacBook.local>
2014-11-28 14:50:04 -0700

    Getting house scenes functional

--- Assets/Scripts/Controller/Objects/Platforms/OneWayPlatform.cs
+++ Assets/Scripts/Controller/Objects/Platforms/OneWayPlatform.cs
@@ -4,31 +4,17 @@ using System.Collections;
 // See:  http://answers.unity3d.com/questions/576044/one-way-platform-using-2d-colliders.html
 public class OneWayPlatform : MonoBehaviour
 {
-	// Use this for initialization
-	void Start ()
-	{
-		transform.parent.collider2D.isTrigger = false; // JIC
 
-	}
-
-	void OnTriggerEnter2D(Collider2D player)
+	void OnCollisionStay2D(Collision2D player)
 	{
-		if(player.tag == "Player")
+		if(player.gameObject.tag == "Player")
 		{
-			// let player pass through
-			this.transform.parent.collider2D.isTrigger = true;
+			if(RebindableInput.GetAxisDown("Vertical") < 0)
+			{
+				this.gameObject.collider2D.isTrigger = true;
+			}
 
 		}
 	}
-
-	//Instead of using the players box collider can we use the "ground check" marks to determine when the platform can be solid again?
-	void OnTriggerExit2D(Collider2D player)
-	{
-		if(player.tag == "Player")
-		{
-			// make platform solid again
-			transform.parent.collider2D.isTrigger = false;
-
-		}
-	}
+	
 }
