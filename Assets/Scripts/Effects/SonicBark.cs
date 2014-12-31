561087e6427a4b0ae845448f1fd998cea093f2e4
magicjedi90 <jesse@JessesMacBook.local>
2014-12-31 10:16:06 -0700

    Created Sonic Bark Prefab

---
+++ Assets/Scripts/Effects/SonicBark.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SonicBark: MonoBehaviour {
+	public float expoDelay = 1f;
+	public float expoRate = 1f;
+	public float expoMaxSize = 10f;
+	public float curRadius = 0f;
+	public float forceMultiplier = 1f;
+
+	bool exploded = false;
+	CircleCollider2D expoRadius;
+
+
+	// Use this for initialization
+	void Start () {
+		expoRadius = this.gameObject.GetComponent<CircleCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		/*expoDelay -= Time.deltaTime;
+		if (expoDelay < 0)
+		{
+			exploded = true;
+		}*/
+		if(Input.GetKeyDown(KeyCode.B))
+		   exploded = true;
+	
+	}
+
+	void FixedUpdate() {
+
+		if(exploded)
+		{
+			if(curRadius < expoMaxSize)
+			{
+				curRadius += expoRate;
+			}
+			else
+			{
+				exploded = false;
+				curRadius = .0001f;
+			}
+			expoRadius.radius = curRadius;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if((col.gameObject.rigidbody2D != null && col.gameObject.layer.ToString() == "Destructible") ||
+		   (col.gameObject.rigidbody2D != null && col.gameObject.tag.ToString() == "Destructible"))
+		{
+
+				Vector2 target = col.gameObject.transform.position;
+				Vector2 boom = gameObject.transform.position;
+
+				Vector2 direction = target - boom;
+
+				float distance = Mathf.Abs(Vector2.Distance(boom, target)); 
+
+				float power = expoMaxSize - distance;
+				if (power < 0)
+					power = 0;
+				Vector2 explosiveForce = direction.normalized * power * forceMultiplier; 
+				col.gameObject.rigidbody2D.AddForce(explosiveForce);
+		}
+
+	}
+
+}
