334636aaf954ace209474f1659e43dcdfa023869
Spiris <Jasontheechidna@cox.net>
2015-01-05 13:15:51 -0700

    Trying to fix this...

--- Assets/Scripts/PointAndClick Script/TrayCheck.cs
+++ Assets/Scripts/PointAndClick Script/TrayCheck.cs
@@ -4,20 +4,29 @@ using System.Collections;
 
 public class TrayCheck : MonoBehaviour
 {
+    GameObject[] slots;
+    bool firstUpdate = true;
     public void UpdateTray()
     {
-        for (int i = 0; i < transform.childCount; i++)
+        if (firstUpdate)
         {
-            GameObject slot = transform.GetChild(i).gameObject;
-
+            slots = new GameObject[transform.childCount];
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                slots [i] = transform.GetChild(i).gameObject;
+            }
+            gameObject.SetActive(false);
+            firstUpdate = false;
+        }
+        for (int i = 0; i < slots.Length; i++)
+        {
             if (i < InventoryManager.data.inventories [0].Items.Count)
             {
-                slot.SetActive(true);
-                Image img = slot.GetComponent<Image>();
+                slots [i].SetActive(true);
+                Image img = slots [i].GetComponent<Image>();
                 img.sprite = InventoryManager.data.inventories [0].GetItemSprite(i);
             } else
-                slot.SetActive(false);
+                slots [i].SetActive(false);
         }
     }
-
 }
