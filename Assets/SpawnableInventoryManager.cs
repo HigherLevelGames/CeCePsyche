7c6e9dd161b6955d54d55e491f32c209549158ce
Spiris <Jasontheechidna@cox.net>
2015-01-05 21:04:25 -0700

    Made the game find ceci and use her transform at the beginning of every level.

--- Assets/SpawnableInventoryManager.cs
+++ Assets/SpawnableInventoryManager.cs
@@ -1,14 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SpawnableInventoryManager : MonoBehaviour {
+public class SpawnableInventoryManager : MonoBehaviour
+{
 
-    public static SpawnableInventoryManager data;
-    public GameObject SqueakyToy;
-    public Transform SpawnPoint;
-	void Awake()
-    {
-        if (data == null)
-            data = this;
-    }
+		public static SpawnableInventoryManager data;
+		public GameObject SqueakyToy;
+		public Transform SpawnPoint;
+
+		void Awake ()
+		{
+				if (data == null)
+						data = this;
+
+		}
+
+		void OnLevelWasLoaded (int level)
+		{
+				SpawnPoint = FindObjectOfType<PlayerInput> ().gameObject.transform;
+		}
 }
