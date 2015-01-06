86a9628c25d07fa41c2ecbfa2e68a888158421d4
magicjedi90 <jesse@JessesMacBook.local>
2015-01-05 19:13:48 -0700

    Started on Natti Prefab

---
+++ Assets/Scripts/Controller/Dogs/BarkSpawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarkSpawner : MonoBehaviour {
+	public GameObject objectToSpawn;
+	public Transform spawnPos;
+
+
+	void Update()
+	{
+		if( Input.GetKeyDown(KeyCode.B))
+			CreateSB();
+	}
+
+	public void CreateSB () 
+	{
+			if(objectToSpawn != null && spawnPos != null)
+			{
+				Vector3 spawnSpot = new Vector3(spawnPos.position.x, spawnPos.position.y, spawnPos.position.z);
+				Instantiate(objectToSpawn,spawnSpot, Quaternion.identity);
+			}
+	}
+}
