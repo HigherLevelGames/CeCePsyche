43516771210dbb5d7aea5f85d7861736666b74d7
Spiris <Jasontheechidna@cox.net>
2015-01-06 12:51:45 -0700

    It's a bird, it's a plane! It's flying text! Here to save the day!

--- Assets/Scripts/Controller/Objects/Interactable/PortalToScene.cs
+++ Assets/Scripts/Controller/Objects/Interactable/PortalToScene.cs
@@ -1,5 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+// This enumeration lists out all of the possible transitions in the game 
+// and keeps them in a "list" of sorts, there needs to be one item for each
+// set of portals. ie. A portal in the Shire that leads to Mordor would need
+// an item: ShireToMordor, which would then be used as the relation on BOTH
+// portals. The program searches for it's paired portal and moves the player
+// accordingly.
+/// <summary>
+/// The relation between a pair of portals used to determine spawn points.
+/// </summary>
 public enum PortalRelation
 {
     None = -1,
@@ -14,8 +23,7 @@ public class PortalToScene : MonoBehaviour, IInteractable
     public PortalRelation relation = PortalRelation.None;
     void Awake()
     {
-        if (relation != PortalRelation.None)
-        if(relation == SpawnManager.relation)
+        if (relation != PortalRelation.None && relation == SpawnManager.relation)
             SpawnManager.PlayerTransform.position = this.gameObject.transform.position;
     }
     public void Interact()
